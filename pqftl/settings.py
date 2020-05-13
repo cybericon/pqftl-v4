23a1d3cf0f11f21586b6775d7e7977054bdedd12
Muhammad Furqan <drmfuran@gmail.com>
2020-05-13 17:07:52 +0500

    cofigured aws s3

--- pqftl/settings.py
+++ pqftl/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django_filters',
     'django.contrib.humanize',
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -131,17 +132,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'pqftl/static/'),
 )
 
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 LOGIN_URL = '/login/'
+
+AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = 'pqftl-hyd-django'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'pqftl.storage_backends.MediaStorage'
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+                       )
+AWS_DEFAULT_ACL = None
--- pqftl/urls.py
+++ pqftl/urls.py
@@ -24,16 +24,11 @@ from dashboard.views import logout_page
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='pqftl/login.html'), name='login'),
-    path('logout/', logout_page, name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+
+    #  Apps Urls
     path('', include('dashboard.urls')),
     path('location/', include('location.urls')),
     path('user/', include('user.urls')),
     path('business/', include('business.urls')),
 ]
-
-
-if settings.DEBUG:
-    # urlpatterns += static(settings.STATIC_URL,
-    #                       document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
